a26e55f3e770ab952c4125d12c0328569c130b10
Karimslayerx <karimslayer7@gmail.com>
2025-02-16 20:37:12 +0100

    Add contact page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image src="/hero skoda.svg" alt="hero" fill className="object-contain" />
         </div>
 
         <div className="hero__image-overlay" />
